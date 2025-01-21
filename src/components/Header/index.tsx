aea2bdc34321072dd84ade73fb9059eae121d636
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-01-20 20:47:19 -0500

    feat: modal de cadastro de transações

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 import logo from "../../assets/logo.svg";
-
-export function Header(){
+import * as Dialog from "@radix-ui/react-dialog"
+import { NewTransactionModal } from "../NewTransactionModal";
+export function Header() {
 
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                   <NewTransactionModal/>
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
